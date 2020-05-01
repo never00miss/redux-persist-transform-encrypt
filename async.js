a1d4e27999c16cdbd4fc25ddaa6ce150b4d6f79c
never00miss <allan.putra@yahoo.com>
2020-05-01 20:59:59 +0700

    Update async.js

--- async.js
+++ async.js
@@ -1,5 +1,7 @@
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
-global.process.version = "12.10.0"
+if(!("version" in global.process)) {
+    global.process.version = "12.10.0"
+}
 
 module.exports = require('./src/async');
